e2813469e5ac73ce24ad69f88aecfb28a1e293c1
liq19ch <s4031630087@gmail.com>
2022-04-12 16:12:38 +0800

    init

---
+++ src/main/java/com/innova/pwValidator/PwValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.innova.pwValidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innova/pwValidator/PwValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.pwValidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
