b56aa10737df3c2d654ed37890b1cfa21826834e
liq19ch <s4031630087@gmail.com>
2022-04-16 14:57:27 +0800

    fixed missing

--- src/test/java/com/innova/pwValidator/PwValidatorApplicationTests.java
+++ src/test/java/com/innova/pwValidator/PwValidatorApplicationTests.java
@@ -1,13 +1,21 @@
 package com.innova.pwValidator;
 
+import com.innova.pwValidator.controller.PwReqController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PwValidatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+	@Autowired
+	private PwReqController controller;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
 }
